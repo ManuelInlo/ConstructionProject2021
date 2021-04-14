09a5dbf76719dd22ad74d1d41717ea83ab6081b9
ManuelInlo <iniguezlopez.devt@gmail.com>
2021-04-14 12:43:23 -0500

    adding a new entitie file

---
+++ Sistema/src/entidades/Integrant.java
@@ -0,0 +1,21 @@
+package entidades;
+
+import java.sql.Date; //Tambien existe la libreria java.util.Date
+
+/**
+ *
+ * @author inigu
+ */
+public class Integrant {
+    private String curp;
+    private String role;
+    private String nameIntegrant;
+    private String studyDegree;
+    private String studyDicipline;
+    private String prodepParticipation;
+    private String typeTeaching;
+    private String eisStudyDegree;
+    private String institucionalMail;
+    private String numberPhone;
+    private Date dateBirthday;
+}
