bbbee42f4573a9bf91d9593857e6035bca006674
DavidMijangos08 <david.mijangos.08@gmail.com>
2021-05-22 18:23:20 -0500

    Ventana login

--- SGP-CA/src/mx/fei/ca/presentation/WindowLoginController.java
+++ SGP-CA/src/mx/fei/ca/presentation/WindowLoginController.java
@@ -7,21 +7,41 @@ package mx.fei.ca.presentation;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
- * @author inigu
+ * @author david
  */
 public class WindowLoginController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TextField tfEmail;
+    @FXML
+    private PasswordField pfPassword;
+
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void openHomePage(ActionEvent event) {
+    }
+
+    @FXML
+    private void closeWindowLogin(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
     
 }
